6626cdd58e1851d1e78abe108bba48a0e9246369
Nguyen Hai Long <ngocnbfb0100097@fpt.edu.vn>
2019-10-29 13:10:43 +0700

    commit dau tien

---
+++ Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  ReadPlistFile
+//
+//  Created by Apple on 10/29/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
+
+
+struct Cities: Codable {
+    var CityCode: Int
+    var Name: String
+}
+
+
